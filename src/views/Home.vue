d3d078cd986b206dee64101dbb016abc1c573298
dark64 <darem966@gmail.com>
2021-03-03 11:51:16 +0100

    add polkadot deps, update webpack config

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,9 +3,3 @@
     <img class="inline-block" alt="Vue logo" src="../assets/logo.png" />
   </div>
 </template>
-
-<script lang="ts">
-import { Vue } from "vue-class-component";
-
-export default class Home extends Vue {}
-</script>
--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,17 @@ const path = require("path");
 
 module.exports = {
   configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.mjs$/,
+          include: /node_modules/,
+          type: 'javascript/auto'
+        }
+      ]
+    },
     resolve: {
+      extensions: ["*", ".vue", ".mjs", ".js", ".json"],
       alias: {
         "@": path.resolve(__dirname, "src"),
       },
