dd253d3bd7789496b11b464407a38c9d7d0b16a6
anamst <ana.milic.strkalj@gmail.com>
2021-04-28 14:17:48 +0200

    alert v2

---
+++ src/components/common/alert/Alert.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="flex items-center justify-between rounded-lg shadow-md px-6 mx-auto mt-2"
+    :class="{
+      'bg-green': alert.type === 'success',
+      'bg-red-500': alert.type === 'error',
+      'bg-yellow-200': alert.type === 'warning',
+    }"
+  >
+    <strong class="font-medium text-white text-base">{{ alert.message }}</strong>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Alert",
+  props: {
+    alert: {
+      type: Object,
+      required: true
+    }
+  },
+};
+</script>
---
+++ src/components/common/alert/AlertContainer.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="fixed w-full top-0 left-0 flex justify-center z-50">
+    <transition name="alert">
+      <Alert v-if="showAlert" />
+    </transition>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import Alert from "./Alert";
+export default {
+  name: "AlertContainer",
+  components: {
+    Alert
+  },
+  setup() {
+    showAlert = ref(false);
+
+    const triggerAlert = () => {
+      showAlert.value = true;
+      setTimeout(() => (showAlert.value = false), 2000);
+    };
+    return { showAlert, triggerAlert };
+  }
+};
+</script>
+
+<style>
+.alert-enter-from {
+  opacity: 0;
+  transform: translateY(-60px);
+}
+.alert-enter-active {
+  transition: all 0.3s ease;
+}
+.alert-leave-to {
+  opacity: 0;
+  transform: translateY(-60px);
+}
+.alert-enter-active {
+  transition: all 0.3s ease;
+}
+</style>
