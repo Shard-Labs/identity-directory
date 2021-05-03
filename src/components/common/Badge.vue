6cd9e8ee0610f1022d79f06ae6faa1028f8c9494
Miljan Milidrag <mm@shardlabs.io>
2021-05-03 14:52:48 +0200

    Application expanded with Badge

---
+++ src/components/common/Badge.vue
@@ -0,0 +1,40 @@
+<template>
+  <div
+    :class="bgColor"
+    class="w-20 h-6 text-xs text-white font-bold rounded-full flex items-center justify-center"
+  >
+    <Icon v-if="prefixIcon" :name="prefixIcon" iconClass="mr-1" />
+    <p>{{ label }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Icon from "@/components/common/Icon.vue";
+
+export default defineComponent({
+  name: "Badge",
+  props: {
+    color: {
+      type: String,
+      required: true
+    },
+    prefixIcon: {
+      type: String,
+      required: false
+    },
+    label: {
+      type: String,
+      required: false
+    }
+  },
+  components: {
+    Icon
+  },
+  computed: {
+    bgColor(): string {
+      return `bg-${this.color}`;
+    }
+  }
+});
+</script>
