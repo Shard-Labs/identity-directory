aa8a867e624072fda5359d69f8e5d385fbaa66cb
Miljan Milidrag <mm@shardlabs.io>
2021-05-17 07:40:21 +0200

    Application expanded with modal

---
+++ src/components/common/Modal.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="backdrop" v-if="show">
+    <div class="modal">
+      <div
+        class="flex justify-between p-5 text-black font-bold text-xl items-center"
+      >
+        {{ header }}
+        <Icon name="close" @click="handleClose"/>
+      </div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import Icon from "@/components/common/Icon.vue";
+
+export default {
+  name: "Modal",
+  components: { Icon },
+  props: {
+    show: {
+      type: Boolean
+    },
+    header: {
+      type: String
+    }
+  },
+  methods: {
+    handleClose() {
+      this.$emit("close");
+    }
+  }
+};
+</script>
+
+<style>
+.backdrop {
+  background-color: rgba(50, 50, 50, 0.5);
+  z-index: 100;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+}
+.modal {
+  position: absolute;
+  min-width: 400px;
+  top: 75px;
+  left: 50%;
+  transform: translateX(-50%);
+  background-color: #fff;
+  border: 2px solid #e6007a;
+  border-radius: 25px;
+  color: #e6007a;
+  z-index: 100;
+}
+</style>
